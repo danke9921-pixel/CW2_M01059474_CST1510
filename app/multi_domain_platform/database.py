f82c6ef8f41a96ce86f87b173ac1e9839bce0878
Djani Colakovic <danke9921@gmail.com>
2025-12-10 15:07:15 +0000

    Resolved DB_PATH so professor can view it

--- app/multi_domain_platform/database.py
+++ app/multi_domain_platform/database.py
@@ -7,7 +7,7 @@
 import sqlite3
 
 #  Direct absolute path to your database
-DB_PATH = r"C:\Users\djani\Documents\MDX Level 4\Programming and Data Communication Networks\CW2_M01059474\CW2_M01059474_CST1510\DATA\intelligence_platforms.db"
+DB_PATH = r"DATA/intelligence_platforms.db"
 
 def connect_database():
     return sqlite3.connect(DB_PATH, check_same_thread=False)
